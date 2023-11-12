0e0af448475b14e20336aefcb3f4b92ae5fefad7
Bruna Dias Pupo <brunapupopupo@gmail.com>
2023-11-12 14:09:27 -0300

    consultas queries mysql

--- knex/config/database.js
+++ knex/config/database.js
@@ -1,6 +1,13 @@
 const knexfile = require("../../knexfile");
+
 const knex = require("knex")(knexfile);
+
+// knex("users").then((data) => {
+//   console.log(data);
+// }); //SELECT * FROM USERS
 
 knex("users").then((data) => {
   console.log(data);
 }); //SELECT * FROM USERS
+
+module.exports = knex;
